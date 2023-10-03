1e5e2aed1a98aad30f77209ef63d61ea8f1cd925
All-Missing <phk6927@autuni.ac.nz>
2023-10-04 00:22:40 +1300

    modify CashierAppTesting1

--- CashierAppTesting1.java
+++ CashierAppTesting1.java
@@ -14,25 +14,28 @@ public class CashierAppTesting1 {
         {
             System.out.print("\tDo you want to start a new shift? (y/n): ");
             System.out.print("> User input: ");
-            String userInput = scan.nextLine();                      
+            String userInput = scan.nextLine();
+            System.out.println("User answer: " + userInput);
            
+            //Check if user's answer is 'n' then exist the entire program
             if (userInput.trim().equalsIgnoreCase("n"))
             {
                System.out.println("User don't wish to login");
                System.exit(0);
             }
                         
-            //NOTE: I can create an obj that will find the last shift_id from BillOrder_Records textFile if
-            //file BillOrder_Records is not null. Else, this will start with shift_id = 0
+            //Check if user's answer is 'y' then process login staff_id 
             if (userInput.trim().equalsIgnoreCase("y"))
             {
                AccessStaffFile accessStaffFile = new AccessStaffFile(shift_id);
                accessStaffFile.loginStaff_ID();
                shift_id++;
-            }                                                                                
-        }
-        
-       
+            }
+            else
+            {   System.out.println("Invalid input, Please answer (y/n) !");               
+            }
+            
+        }              
     }
     
 }
