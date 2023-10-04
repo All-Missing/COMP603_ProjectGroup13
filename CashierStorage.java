a306445d41f6df1967c2fb69cf368ce202b84234
All-Missing <phk6927@autuni.ac.nz>
2023-10-04 17:07:05 +1300

    modify CashierStorage - create STAFF database

--- CashierStorage.java
+++ CashierStorage.java
@@ -14,44 +14,39 @@ public class CashierStorage {
     
     private final CashierDBManager dbManager;
     private final Connection conn;
-    private Statement statement;
-//    private HashMap<String, Product> product_records;
+    private Statement statement;    
     private ProductList productList;
+    private Staff_Record staffList;
     
     public CashierStorage() {
         dbManager = new CashierDBManager();
-        conn = dbManager.getCashierDBConnection();
-        productList = new ProductList();        
+        conn = dbManager.getCashierDBConnection();        
+        productList = new ProductList();
+        staffList = new Staff_Record();
     }
     
+    //Create PRODUCT SQL table
     public void createProductDB() {
         
         try {
             //connect and initialize db
             statement = conn.createStatement();
         
-            String tableName = "PRODUCT";
-             //Check if table exisits. if not, manually create it
-            checkExistedTable(tableName);
+            String tableName = "PRODUCT";             
+            checkExistedTable(tableName); //Check if table exisits. if not, manually create it
             
             String sqlTable = "CREATE TABLE " + tableName + " (ITEM_ID VARCHAR(10), "
                     + "ITEM VARCHAR(50), ITEM_PRICE DOUBLE, CATEGORY VARCHAR(20))";                    
             statement.executeUpdate(sqlTable);
             System.out.println("Table " + tableName +" created");
                        
-            //Retrieve product records value
-            HashMap<String, Product> product_records = productList.getProduct_records();
-            
-            //Insert sql table
+            //Retrieve product records value and insert data into sql table
+            HashMap<String, Product> product_records = productList.getProduct_records();                        
             for (Map.Entry<String, Product> entry: product_records.entrySet()) {
                 
                 String item_id = entry.getKey();
                 Product product = entry.getValue();
                 
-//                System.out.println("Item_id: "+item_id+" "
-//                                            +"Item: "+product.getItem()+" "
-//                                            +"Item Price: "+product.getItemPrice()+" "
-//                                            +"Catagory: "+product.getCategory());
                 String insertQuery = "INSERT INTO " + tableName + " VALUES ('"
                         + item_id + "', '"
                         + product.getItem() + "', "
@@ -59,18 +54,45 @@ public class CashierStorage {
                         + product.getCategory() + "')";
                          
                 statement.executeUpdate(insertQuery);
-            }
-            
-            
+            }                        
         } catch (SQLException ex) {
             Logger.getLogger(CashierStorage.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        
-        
-    }
+        }          
+    }        
     
-    public void checkExistedTable(String aTableName) {
+    //Create STAFF SQL table
+    public void createStaffDB() {
+            
+        try {
+            //connect and initialize db
+            statement = conn.createStatement();
+        
+            String tableName = "STAFF";             
+            checkExistedTable(tableName); //Check if table exisits. if not, manually create it
+            
+            String sqlTable = "CREATE TABLE " + tableName + " (STAFF_ID VARCHAR(10), STAFF_NAME VARCHAR(20))";
+                               
+            statement.executeUpdate(sqlTable);
+            System.out.println("Table " + tableName +" created");
+                       
+            //Retrieve product records value and insert data into sql table
+            HashMap<String, String> staff_records = staffList.getStaff_list();
+            for (Map.Entry<String, String> entry: staff_records.entrySet()) {
+                
+                String staff_id = entry.getKey();
+                String staff_name = entry.getValue();
+                
+                 String insertQuery = "INSERT INTO " + tableName + " VALUES ('"
+                        + staff_id + "', '"                       
+                        + staff_name + "')";                         
+                statement.executeUpdate(insertQuery);
+            }                        
+        } catch (SQLException ex) {
+            Logger.getLogger(CashierStorage.class.getName()).log(Level.SEVERE, null, ex);
+        }          
+    } 
+    
+    private void checkExistedTable(String aTableName) {
         
         try {
             DatabaseMetaData dbmd = this.conn.getMetaData();
