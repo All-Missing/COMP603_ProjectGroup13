9ae032cc1948fa4257bc177056171cbd847ea9bd
All-Missing <phk6927@autuni.ac.nz>
2023-10-04 14:43:33 +1300

    add CashierStorageRun

---
+++ CashierStorageRun.java
@@ -0,0 +1,12 @@
+package COMP603_ProjectGroup13;
+
+
+public class CashierStorageRun {
+    
+    
+    public static void main(String[] args) {
+        CashierStorage cashierStorage = new CashierStorage();
+        cashierStorage.createProductDB();
+    }
+    
+}
