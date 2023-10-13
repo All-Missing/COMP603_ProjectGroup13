f68c3e1508ef12eec89125fadaaf25898b3c08ab
All-Missing <phk6927@autuni.ac.nz>
2023-10-14 01:49:35 +1300

    add CheckShiftID

---
+++ CheckShiftID.java
@@ -0,0 +1,31 @@
+package COMP603_ProjectGroup13;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class CheckShiftID {                   
+    
+    public int checkShiftID() {
+        
+        int highestShiftID = 1;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("./file_records/BillOrder_Records.txt"));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("---ShiftID: ")) {
+                    int currentShiftID = Integer.parseInt(line.split(" ")[1]);
+//                    System.out.println("Found ShiftID: " + currentShiftID); // Debug statement
+                    if (currentShiftID > highestShiftID) {
+                        highestShiftID = currentShiftID;
+                    }
+                }
+            }
+//            System.out.println("Calculated Next ShiftID: " + (highestShiftID + 1)); // Debug statement
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return highestShiftID + 1;
+    }
+}
