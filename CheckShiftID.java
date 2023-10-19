86f94dc786467d68be52f258699c0eaea8af9f7d
All-Missing <phk6927@autuni.ac.nz>
2023-10-20 09:02:24 +1300

    modify CheckShiftID

--- CheckShiftID.java
+++ CheckShiftID.java
@@ -10,7 +10,7 @@ class CheckShiftID {
         
         int highestShiftID = 0;
         try {
-            BufferedReader br = new BufferedReader(new FileReader("./file_records/BillOrder_Records"));
+            BufferedReader br = new BufferedReader(new FileReader("./file_records/BillOrder_Records.txt"));
             String line = "";
             while ((line = br.readLine()) != null) {
                 if (line.startsWith("---ShiftID: ")) {
